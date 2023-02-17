379a490f752624890b3922cae07bead27d5c7bd4
Jameel Ibrahim <jameelibrahim@Jameels-Air.hsd1.ct.comcast.net>
2023-02-17 14:18:30 -0500

    Updated src

--- src/01-forEach.js
+++ src/01-forEach.js
@@ -12,7 +12,14 @@ const exampleSongData = require("../data/songs");
  * Logs out all of the song titles.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
-function printAllSongTitles(songs) {}
+function printAllSongTitles(songs) {
+  
+  
+  const song = (songs) => {
+    console.log(songs.title);
+  }
+  songs.forEach(song);
+}
 
 /**
  * Logs out the song title and song artist.
@@ -24,13 +31,25 @@ function printAllSongTitles(songs) {}
  *  //> "Up by Sebastian Kamae"
  *  //> ...
  */
-function printSongDetails(songs) {}
+function printSongDetails(songs) {
+  const song = (songs) => {
+    console.log(`${songs.title} by ${songs.artist}`);
+  }
+  songs.forEach(song);
+}
 
 /**
  * Logs out all of the song titles which have a runtime over three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
-function printSongTitlesOverThreeMinutes(songs) {}
+function printSongTitlesOverThreeMinutes(songs) {
+  const song = songs => {
+    if (songs.runtimeInSeconds > 180) {
+      console.log(songs.title);
+    }
+  }
+  songs.forEach(song);
+}
 
 module.exports = {
   printAllSongTitles,
