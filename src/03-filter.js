59885a47937d7ed0f89b60b1a6b8954d62f5b3e7
Blui322 <luistejada@pursuit.com>
2023-02-19 13:36:30 -0500

    this one was tough and i loved it

--- src/03-filter.js
+++ src/03-filter.js
@@ -13,21 +13,40 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getSongsBySaib(songs) {}
+function getSongsBySaib(songs) {
+   return songs.filter((element) => {
+    if (element.artist === "Saib"){
+      return element;
+    }
+  });
+}
 
 /**
  * Returns an array of all songs where the runtime is over three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getSongsOverThreeMinutes(songs) {}
+function getSongsOverThreeMinutes(songs) {
+  return songs.filter((element) => {
+    if (element.runtimeInSeconds > 180){
+      return element.title
+    }
+  })
+}
 
 /**
  * Returns an array of songs where the song title is the same as the song album.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getTitleTracks(songs) {}
+function getTitleTracks(songs) {
+  return songs.filter((element) => {
+    if(element.title === element.album){
+      return element
+      
+    }
+  })
+}
 
 module.exports = {
   getSongsBySaib,
