32d5dfa9a72bce86c38070c626dc535b8c3053a5
rboyce212 <richboyce@pursuit.org>
2023-02-20 17:12:36 -0500

    finished 04 - find

--- src/04-find.js
+++ src/04-find.js
@@ -13,24 +13,35 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findPinkElephantsByTimestreet(songs) {}
+function findPinkElephantsByTimestreet(songs) {
+  let pinkElephantsSong = songs.find((song) => song.title === "Pink Elephants");
+  return pinkElephantsSong;
+}
 
 /**
  * Returns the first song in the list that is under three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findFirstSongUnderThreeMinutes(songs) {}
+function findFirstSongUnderThreeMinutes(songs) {
+  let songUnderThreeMinutes = songs.find((song) => song.runtimeInSeconds < 180);
+  return songUnderThreeMinutes;
+}
 
 /**
  * Returns the first song in the list where the song title equals the song album.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findFirstTitleTrack(songs) {}
+function findFirstTitleTrack(songs) {
+  let songTitleEqualsAlbumTitle = songs.find(
+    (song) => song.title === song.album
+  );
+  return songTitleEqualsAlbumTitle;
+}
 
 module.exports = {
   findPinkElephantsByTimestreet,
   findFirstSongUnderThreeMinutes,
-  findFirstTitleTrack,
+  findFirstTitleTrack
 };
