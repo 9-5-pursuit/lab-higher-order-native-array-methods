a1a781333ac8bb5be0d3124cc30f8d083611ffaa
Emely Martinez-Pichardo <emelymp98@gmail.com>
2023-03-10 00:51:23 -0500

    done with 04 all tests passed

--- src/04-find.js
+++ src/04-find.js
@@ -13,21 +13,49 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findPinkElephantsByTimestreet(songs) {}
+
+// returns a single obj that is this one song
+function findPinkElephantsByTimestreet(songs) {
+
+// const findPinkElephantsByTimestreet = (songs) => songs.find((findSong) = findSong.title === "Pink Elephants")
+// console.log(findPinkElephantsByTimestreet(exampleSongData));
+
+  return songs.find((findSong) => findSong.title === "Pink Elephants")
+  
+}
+
+// console.log(findPinkElephantsByTimestreet(exampleSongData));
 
 /**
  * Returns the first song in the list that is under three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findFirstSongUnderThreeMinutes(songs) {}
+
+// returns FIRST song in list that is under three mins
+// < 180
+function findFirstSongUnderThreeMinutes(songs) {
+
+  return songs.find((findSong) => findSong.runtimeInSeconds < 180);
+
+}
+
+// console.log(findFirstSongUnderThreeMinutes(exampleSongData));
 
 /**
  * Returns the first song in the list where the song title equals the song album.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findFirstTitleTrack(songs) {}
+
+// returns FIRST song in list where song title === song album
+function findFirstTitleTrack(songs) {
+
+  return songs.find((findSong) => findSong.title === findSong.album);
+
+}
+
+// console.log(findFirstTitleTrack(exampleSongData));
 
 module.exports = {
   findPinkElephantsByTimestreet,
